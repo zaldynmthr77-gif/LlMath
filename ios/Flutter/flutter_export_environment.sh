702b1cb9034911fb3b5994d11ad0916039cdb368
zaldynmthr77-gif <zaldynmthr77@gmail.com>
2025-12-02 07:27:28 +0300

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\User\math_game"
+export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\math_game"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
